435a70f5268c1976a8e30096cf9039cc1b7a27a6
Miyu Togo <togomiyu@gmail.com>
2022-02-16 23:57:35 +0800

    Sum Function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [a, setA] = useState ();
+  const [b, setB] = useState ();
+  const [c, setC] = useState ();
+  const [value, setValue] = useState ();
+
+  const sum = (a,b,c) => {
+   
+    if ( a == '' || b == ''  || c == '') {
+      // console.log(a)
+      // console.log(b)
+      // console.log(c)
+      // console.log('Missing parameter') 
+      setValue('Missing parameter')
+    } else {
+      const total = Number(a) + Number(b) + Number(c);
+      setValue(total)
+
+      console.log(c)
+    } 
+    
+  }
+  // console.log(a)
+  // console.log(b)
+  // console.log(c)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <p>Sum Function</p>
+      <input type='number'onChange={(e) => setA(e.target.value)} ></input> 
+      <br/>
+      <input type='number' onChange={(e) => setB(e.target.value)} ></input> 
+      <br/>
+      <input type='number' onChange={(e) => setC(e.target.value)}></input> 
+      <br/><br/>
+      <button onClick={()=> sum(a, b, c)}>GET SUM </button>
+
+      <p>{value}</p>
+      </center>
     </div>
   );
 }
